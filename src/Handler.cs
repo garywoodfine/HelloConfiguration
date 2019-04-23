ee2dc57c488e9bee9eb8a384dcbc72c774f1bb35
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-04-23 15:11:32 +0100

    Basic DI and Config implemented

--- src/Handler.cs
+++ src/Handler.cs
@@ -1,5 +1,6 @@
 using Amazon.Lambda.Core;
 using System;
+using Microsoft.Extensions.DependencyInjection;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -7,6 +8,24 @@ namespace AwsDotnetCsharp
 {
     public class Handler
     {
+        private ILambdaConfiguration Configuration { get; }
+        public Handler()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            Configuration = serviceProvider.GetService<ILambdaConfiguration>();
+        }
+        
+        public string Hello( ILambdaContext context)
+        {
+            return Configuration.Configuration["hello"] ?? "None";
+        }
+        
+        private void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<ILambdaConfiguration, LambdaConfiguration>();
+        }
       
     }
 }
--- src/LambdaConfiguration.cs
+++ src/LambdaConfiguration.cs
@@ -1,9 +1,15 @@
+using System.IO;
 using Microsoft.Extensions.Configuration;
 
 namespace AwsDotnetCsharp
 {
     public class LambdaConfiguration : ILambdaConfiguration
     {
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration  => new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+
     }
 }
